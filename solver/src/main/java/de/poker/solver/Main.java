fb39595d4c915107ef08fafa76484788725c2894
Benjamin <benjamin-perkins@outlook.com>
2022-07-19 20:28:50 +0200

    Added improvement todos

--- solver/src/main/java/de/poker/solver/Main.java
+++ solver/src/main/java/de/poker/solver/Main.java
@@ -1,5 +1,12 @@
 package de.poker.solver;
 
+// TODO Multithreading
+// TODO persist data
+// TODO actions are determined based on config file which can be read at runtime
+// TODO load data from checkpoint
+// TODO Trainer does discounting
+// TODO write tests for everything
+// TODO maybe make stuff for functional
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         UserInterface userInterface = new UserInterface();
