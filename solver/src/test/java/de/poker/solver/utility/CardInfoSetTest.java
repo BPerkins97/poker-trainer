6f27586da39ccbb166a62a7129aef560dcce651f
Benjamin <benjamin-perkins@outlook.com>
2022-07-24 10:16:30 +0200

    Fixed hand normalization

--- solver/src/test/java/de/poker/solver/utility/CardInfoSetTest.java
+++ solver/src/test/java/de/poker/solver/utility/CardInfoSetTest.java
@@ -1,6 +1,8 @@
 package de.poker.solver.utility;
 
 import de.poker.solver.game.Card;
+import de.poker.solver.game.Suit;
+import de.poker.solver.game.Value;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +14,7 @@ import java.util.Set;
 public class CardInfoSetTest {
 
     @Test
-    public void testCases() {
+    public void testHoleCards() {
         Set<Long> longs = new HashSet<>();
 
         for (Card card1 : Card.CARDS) {
@@ -26,5 +28,33 @@ public class CardInfoSetTest {
         }
 
         Assertions.assertEquals(169, longs.size());
+    }
+
+    @Test
+    public void testFlop() {
+        Set<Long> longs = new HashSet<>();
+
+        Card holeCard1 = Card.of(Value.ACE, Suit.CLUB);
+        Card holeCard2 = Card.of(Value.KING, Suit.CLUB);
+
+        for (Card card1 : Card.CARDS) {
+            if (card1.equals(holeCard1) || card1.equals(holeCard2)) {
+                continue;
+            }
+            for (Card card2 : Card.CARDS) {
+                if (card1.equals(card2) || card2.equals(holeCard1) || card2.equals(holeCard2)) {
+                    continue;
+                }
+                for (Card card3 : Card.CARDS) {
+                    if (card3.equals(card1) || card3.equals(card2) || card3.equals(holeCard1) || card3.equals(holeCard2)) {
+                        continue;
+                    }
+                    long result = CardInfoSetBuilder.toInfoSet(Arrays.asList(holeCard1, holeCard2, card1, card2, card3));
+                    longs.add(result);
+                }
+            }
+        }
+
+        Assertions.assertEquals(4650, longs.size());
     }
 }
